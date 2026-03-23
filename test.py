18169f6cea86f3782d1bd306b0988d1e1c723224
singh-damanpreet04 <singh04damanpreet@gmail.com>
2026-03-23 18:28:13 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-books = pd.read_csv('books.csv')
-print(books.head())
 
-print(books['Rating'].corr(books['Price']))
-sns.lineplot(books, x='Rating', y='Price')
-plt.show()
+data = {
+  'one':1,
+  'two':2,
+  'three':3
+}
+
+df = pd.DataFrame(data)
+print(data)
